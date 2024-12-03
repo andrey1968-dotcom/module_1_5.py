86e91341282221cb890875c2aa97026831de9de2
andrey1968-dotcom <cushvintsev36@gmail.com>
2024-12-03 15:55:47 +0500

    Add files via upload

---
+++ 'module_1_5.py
@@ -0,0 +1,13 @@
+immutable_var = (87.23, 67, 4, False, "Boolean", [8, 2, 71])
+print(immutable_var)
+immutable_var[5][1] = 4
+print(immutable_var)
+#immutable_var[0] = 9.2
+#print(immutable_var)
+mutable_list = [98.45, "False", True, 3, (0, 1)]
+print(mutable_list)
+mutable_list[4] = 'Boolean'
+print(mutable_list)
+
+
+
